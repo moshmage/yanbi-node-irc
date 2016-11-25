f2eb2e6f633fd37864187bd52fa3cc37138ff330
MoshMage <moshmage@gmail.com>
2016-11-25 21:00:44 +0000

    adds a minor irc-client check

--- es6/classes/irc-context.js
+++ es6/classes/irc-context.js
@@ -5,6 +5,7 @@ const defaultHooks = require('helpers/default-hooks.js');
 
 class IrcContext {
     constructor(irc, ircConfig) {
+        if (!irc.Client) throw Error('Provided IRC Module does not appear to be a node-irc');
         this.client = irc.Client(ircConfig.server, ircConfig.nick, ircConfig);
         this.client.addListener('error', (message) => console.log(message));
         this.events = new Events();
