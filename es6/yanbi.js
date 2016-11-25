8d5796243cf6d9d5d5d0dffb7778918d3cba412c
MoshMage <moshmage@gmail.com>
2016-11-25 21:10:46 +0000

    connects the dots

--- es6/yanbi.js
+++ es6/yanbi.js
@@ -2,10 +2,19 @@
  * Created by Mosh Mage on 11/25/2016.
  */
 const irc = require('irc');
-const fs = require('fs');
+const IrcContext = require('classes/irc-context.js');
+const ModuleManager = require('classes/module-manager.js');
 
-const IrcConfig = require('config/irc.config');
-const YanbiConfig = require('config/yanbi.config');
-const IrcContext = require('classes/irc-context');
-const ModuleManager = require('classes/module-manager');
+class Yanbi {
+    constructor() {
+        const IrcConfig = require('config/irc.config.js');
+        const YanbiConfig = require('config/yanbi.config.js');
 
+        this.yanbiConfig = YanbiConfig;
+        this.ircContext = new IrcContext(irc, IrcConfig);
+        this.moduleManager = new ModuleManager(this.ircContext.events, this.yanbiConfig);
+        this.moduleManager.loadFromFolder();
+    }
+}
+
+module.exports = Yanbi;
