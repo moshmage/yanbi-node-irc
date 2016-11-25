21cac63d4b7c87f1450e96f8f518d77c2242c174
MoshMage <moshmage@gmail.com>
2016-11-25 19:04:15 +0000

    dispatcher fully refactored

--- es6/classes/irc-context.js
+++ es6/classes/irc-context.js
@@ -1,11 +1,14 @@
 /**
  * Created by Mosh Mage on 11/25/2016.
  */
-"use strict";
+import {defaultHooks} from 'helpers/default-hooks.js'; 
+
 export class IrcContext {
     constructor(irc, ircConfig) {
         this.client = irc.Client(ircConfig.server, ircConfig.nick, ircConfig);
         this.client.addListener('error', (message) => console.log(message));
         this.events = new Events();
+
+        defaultHooks.create(this.events);
     }
 }
