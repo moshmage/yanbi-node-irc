ac98047686f48cc3df2cf8dc6c7720bceaf02cfa
MoshMage <moshmage@gmail.com>
2016-11-25 23:40:59 +0000

    ye.. i know.

--- yanbi.js
+++ yanbi.js
@@ -2,18 +2,17 @@
  * Created by Mosh Mage on 11/25/2016.
  */
 const irc = require('irc');
-const IrcContext = require('classes/irc-context.js');
-const ModuleManager = require('classes/module-manager.js');
+const IrcContext = require('./classes/irc-context.js');
+const ModuleManager = require('./classes/module-manager.js');
 
 class Yanbi {
     constructor() {
-        const IrcConfig = require('config/irc.config.js');
-        const YanbiConfig = require('config/yanbi.config.js');
+        const IrcConfig = require('./config/irc.config.js');
+        const YanbiConfig = require('./config/yanbi.config.js');
 
         this.yanbiConfig = YanbiConfig;
         this.ircContext = new IrcContext(irc, IrcConfig);
         this.moduleManager = new ModuleManager(this.ircContext.events, this.yanbiConfig);
-        this.moduleManager.loadFromFolder();
     }
 }
 
