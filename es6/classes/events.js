210a586b0eccc41b9744f9e6ccec8e8c36ca4c5a
MoshMage <moshmage@gmail.com>
2016-11-25 19:03:17 +0000

    event matching is now a method of the child event itself

--- es6/classes/events.js
+++ es6/classes/events.js
@@ -17,91 +17,100 @@ class Event {
 
         if (!this.match) throw Error('Event needs a match parameter of type string string or object of {word: string, place: number}');
     }
+
+    matches(input, withIndex) {
+        input = new RegExp(input,i);
+        if (!withIndex) return this.matchString.search(input) > -1;
+        return input.search(input) === this.onIndex;
+    }
 }
 
 export class Events {
     constructor(client) {
-        this.typeList = {};
-        this.typeEvents = {};
+        this.parent = {};
+        this.childs = {};
         this.client = client;
     }
 
     /**
      * Adds a Parent Event as EventListener of Irc.Client() object
-     * @param eventType {string}
+     * @param eventName {string}
      * @param callback {function}
      * @param once {boolean}        use .once instead of addListener
      * @void
      */
-    addType(eventType, callback, once) {
-        if (typeof this.typeList[eventType] === 'EventType' && !once) return false;
+    addType(eventName, callback, once) {
+        if (typeof this.parent[eventName] === 'EventType' && !once) return false;
 
         if (once) {
-            this.client.once(eventType, callback);
+            this.client.once(eventName, callback);
         } else {
-            this.typeList[eventType] = new EventType(eventType, callback);
-            this.typeEvents[eventType] = [];
-            this.client.addListener(eventType, this.eventTypes[eventType].callback);
+            this.parent[eventName] = new EventType(eventName, callback);
+            this.childs[eventName] = [];
+            this.client.addListener(eventName, this.eventTypes[eventName].callback);
         }
-        console.log(`Info: Created ${eventType} type: ${(once) ? 'once' : 'forever'}`);
+        console.log(`Info: Created ${eventName} type: ${(once) ? 'once' : 'forever'}`);
     }
 
     /**
      * Removes a Type Event and its children
-     * @param eventType {string}
+     * @param eventName {string}
      * @param callback {Function}
      */
-    removeType(eventType, callback) {
-        if (this.created(eventType)) {
-            this.client.removeListener(eventType);
-            delete this.typeList[eventType];
-            delete this.typeEvents[eventType];
+    removeType(eventName, callback) {
+        if (this.created(eventName)) {
+            this.client.removeListener(eventName);
+            delete this.parent[eventName];
+            delete this.childs[eventName];
         }
 
         if (callback) callback({
-            removed: !this.created(eventType),
-            type: eventType
+            removed: !this.created(eventName),
+            type: eventName
         });
     }
 
     /**
      * Creates a new Child Event inside the Event Type
-     * @param eventType {string}
+     * @param eventName {string}
      * @param matchObject {string|object}          "string" or {word: "string", place: 0}
      * @param callback {Function}
      * @void
      */
-    listen(eventType, matchObject, callback) {
-        if (!this.created(eventType)) throw Error(`No such EventType: ${eventType}`);
+    listen(eventName, matchObject, callback) {
+        if (!this.created(eventName)) throw Error(`No such EventType: ${eventName}`);
         let event = new Event(matchObject, callback);
-        this.typeEvents[eventType].push(event);
-        console.log(`Info: Listening for ${event.matchString} at ${event.onIndex} when ${eventType}`);
+        this.childs[eventName].push(event);
+        console.log(`Info: Listening for ${event.matchString} at ${event.onIndex} when ${eventName}`);
     }
 
     /**
      * releases a previously hooked event
-     * @param eventType {string}
+     * @param eventName {string}
      * @param matchObject {string|object}          "string" or {word: "string", place: 0}
      * @param allFromType {boolean}
      */
-    mute(eventType, matchObject, allFromType) {
-        if (!this.created(eventType)) throw Error(`No such EventType: ${eventType}`);
+    mute(eventName, matchObject, allFromType) {
+        if (!this.created(eventName)) throw Error(`No such EventType: ${eventName}`);
         let count = 0;
         if (allFromType) {
-            this.typeEvents[eventType] = [];
+            this.childs[eventName] = [];
         } else {
-            this.typeEvents[eventType].forEach((event, index) => {
+            this.childs[eventName].forEach((event, index) => {
                 let match = matchObject && matchObject.word || matchObject;
                 let onIndex = matchObject && matchObject.place || 0;
                 if (event.matchString === match && event.onIndex === onIndex) {
-                    this.typeEvents.splice(index, 1);
+                    this.childs.splice(index, 1);
                     count++;
                 }
             });
         }
-        
-        console.log(`Info: Removed ${(allFromType) ? 'all' : count} from ${eventType} when ${matchObject}`);
+
+        console.log(`Info: Removed ${(allFromType) ? 'all' : count} from ${eventName} when ${matchObject}`);
     }
 
-    created(eventType) {return typeof this.typeList[eventType] === 'EventType'};
+    created(eventName) {return typeof this.parent[eventName] === 'EventType'};
+
+    getType(eventName) { return this.created(eventName) && this.parent[eventName]; }
+    getChilds(eventName) { return this.childs[eventName]; }
 }
