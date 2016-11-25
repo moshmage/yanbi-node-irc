ef5c5fd6a8de18edde450782fc6ab46e7cb3df09
MoshMage <moshmage@gmail.com>
2016-11-25 21:08:00 +0000

    makes sense that the irc options are hooked to the irc context instead

--- es6/classes/irc-context.js
+++ es6/classes/irc-context.js
@@ -7,9 +7,9 @@ const Events = require('./events.js');
 class IrcContext {
     constructor(irc, ircConfig) {
         if (!irc.Client) throw Error('Provided IRC Module does not appear to be a node-irc');
+        this.ircConfig = IrcConfig;
         this.client = irc.Client(ircConfig.server, ircConfig.nick, ircConfig);
         this.events = new Events(this.client);
-
         defaultHooks.create(this.events);
     }
 }
