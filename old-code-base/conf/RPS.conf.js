9380e1f7980cb958d1e23db930d0e5bda1ed3124
MoshMage <moshmage@gmail.com>
2016-11-26 01:17:28 +0000

    removes old code base

--- old-code-base/conf/RPS.conf.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Mosh Mage on 5/22/2016.
- */
-module.exports = {
-    writePath: __dirname + '/records/rps.json',
-    RESULTSCHANNEL: '#mmdev'
-};
--- old-code-base/init.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Mosh Mage on 5/25/2016.
- */
-require('./yanbi.js')();
