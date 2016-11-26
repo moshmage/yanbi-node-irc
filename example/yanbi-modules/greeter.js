2093058c7bed19a70388b19f9b8e9c75131dafe9
MoshMage <moshmage@gmail.com>
2016-11-26 00:49:26 +0000

    adds initializer and export to greet module

--- example/yanbi-modules/greeter.js
+++ example/yanbi-modules/greeter.js
@@ -6,13 +6,19 @@ class Greeter {
         this.name = "Greeter";
         this.version = "1.0";
         this.author = "moshmage@gmail.com";
-        
+        this.events = null;
+    }
+
+    initialize(events) {
         this.events = events;
         this.events.listen('join', '#yanbi', this.greetNick);
-
     }
-    
+
+    rehasher() {}
+
     greetNick(channel, nick) {
         this.events.client.say(channel, `Hello, ${nick}`);
     }
 }
+
+module.exports = Greeter;
